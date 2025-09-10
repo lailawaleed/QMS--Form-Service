601f00ea6dcc3bfc14a020cb34b887b78895583c
lailaclear <lailawlaeed@gmail.com>
2025-09-10 21:02:06 +0300

    edit RequestMapping in SeverityController

--- src/main/java/com/example/FormService/controller/SeverityController.java
+++ src/main/java/com/example/FormService/controller/SeverityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/severities")
+@RequestMapping("/api/severities")
 public class SeverityController {
 
     private final SeverityService service;
